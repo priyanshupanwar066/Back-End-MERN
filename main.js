d2329c960acba52ac47dca21fa80c50c889a952d
Priyanshu Panwar <priyanshugurjar066@gmail.com>
2024-12-19 19:34:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,62 @@
-const fs = require("fs");
-
-// console.log(fs);
-
-console.log("Starting");
-// fs.writeFileSync("priyu.txt" , "Priyu is a Good boy")
+const express = require('express')
+const app = express()
+const port = 3000
 
 
-fs.writeFile("priyu2.txt" , "Priyanshu Panwar", ()=>{ 
-    console.log("done");
-    fs.readFile("priyu2.txt" , (error , data)=>{
-        console.log(error , data.toString());
-        
-    } )
+app.use(express.static('public'))
+
+//app.get , or app.post , or app.put , or app.delete (path , handler)
+app.get('/', (req, res) => {
+  res.send('Hello World 22!')
 })
 
 
-fs.appendFile("priyu2.txt" , "priyu 65675", (e, d)=>{
-    console.log(d);
+app.get('/about' , (req, res) => {
+    res.send('About Page')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Contact Page')
+})
+
+app.get('/products', (req, res) => {
+    res.send('Products Page')
+})
+
+app.get('/admin', (req, res) => {
+    res.send('Priyanshu Panwar ')
+})
+
+
+app.get('/blog' , (req, res) => {
+    res.send('Blog Page')
+})
+
+
+app.get("/blog/:slug" , (req , res )=>{
+    //logic to fetch {slug} from the db
+
+    console.log(req.params); //will output { slug: 'priya' }
+    console.log(req.query); //will output { mode: 'dark', 'region-in': ''}
+
     
-})
-console.log("ending");
+    res.send(`hello ${req.params.slug}`)
 
+    // res.send(`hello ${req.params.slug}  and ${req.params.second}`)
+})
+
+
+
+// app.get("/blog/intro-to-js" , (req , res )=>{
+//     //logic to fetch intro to js from the db
+//     res.send("Intro to the Express Js in JavaScript")
+// })
+
+// app.get("/blog/intro-to-py" , (req , res )=>{
+//     //logic to fetch intro to py from the db
+//     res.send("Intro to the python")
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
