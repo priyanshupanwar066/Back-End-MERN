241d88716627c99148772bf30418873ec1cec414
Priyanshu Panwar <priyanshugurjar066@gmail.com>
2024-12-21 12:11:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-// You have to write the Node.js Programs TO clearn clutter inside of a diretory and organize the content of that directory into diffrenet folder
+const express = require("express");
+const app = express();
+const port = 4000
 
-// for example , these files becomes 
+app.set('view engine', 'ejs'); 
+
+// https://github.com/mde/ejs/wiki/Using-EJS-with-Express
 
 
-// 1. name.jpg
-// 2. name.png
-// 3. this.pdf
-// 4. priyu.zip
-// 5. Panwar.jpg
+app.get('/', (req, res) => {
+    let siteName  = "Adidas"
+    let searchText = "Search Now"
+    let arr = [1 ,2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11]
+    res.render('index', {siteName: siteName , searchText: searchText , arr})
+})
 
-//this:
-// jpg/name.jpg , jpg/cat.jpg
 
-// png/name.png
-// zip/priyu.zip , 
+
+app.get ('/blog/:slug' , (req, res) => {
+    let blogTitle = "Adidas Why And When?"
+    let blogContent = "Adidas has been a global sportswear brand for over 20 years, with a strong commitment to innovation and sustainability. It started as a small clothing company in the 1970s, and has since diversified into various brands, including footwear, clothing, and accessories."
+    res.render('blogpost', {blogTitle: blogTitle , blogContent: blogContent})
+})
+
+app.listen(port , ()=>{
+    console.log(`Port is running on ${port}`);
+})
