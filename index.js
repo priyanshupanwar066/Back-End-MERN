c65f002d0a2ab3d466925cbb22d586c97e6a3ecd
Priyanshu Panwar <priyanshugurjar066@gmail.com>
2024-12-21 12:11:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// You have to write the Node.js Programs TO clearn clutter inside of a diretory and organize the content of that directory into diffrenet folder
+
+// for example , these files becomes 
+
+
+// 1. name.jpg
+// 2. name.png
+// 3. this.pdf
+// 4. priyu.zip
+// 5. Panwar.jpg
+
+//this:
+// jpg/name.jpg , jpg/cat.jpg
+
+// png/name.png
+// zip/priyu.zip , 
