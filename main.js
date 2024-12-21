57d5482a1f0bae34e4392f0d356d9eac67731b9d
Priyanshu Panwar <priyanshugurjar066@gmail.com>
2024-12-21 12:14:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,32 @@
-const express = require("express");
-const app = express();
-const port = 4000;
-const fs = require("fs");
-let date = new Date();
-const blog = require('./routes/blog')
+// https://www.npmjs.com/package/mongodb
+import mongoose from "mongoose";
+import express from "express";
+import { Todo } from "./models/Todo.js";
 
-
-// app.use(express.static("public"));
-
-app.use('/blog', blog)
-
-
-//middleware one = logger for application
-app.use((req, res, next) => {
-    console.log(req.headers);
-    req.priyu = "I AM Priyanshu Panwar";
-  fs.appendFileSync("logs.txt", `${date} is a ${req.method}\n`);
-  console.log(`${date} is a ${req.method}`);
-  next();
-});
-
-//Middleware two
-app.use((req, res, next) => {
-  console.log("m2");
-  next();
-});
+let conn = await mongoose.connect("mongodb://127.0.0.1:27017/todo")
+const app = express()
+const Port = 3000
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+    const todo = new Todo({
+        title: "1",
+        desc: "Hellow new row",
+        isDone: false,
+        days: 1227727
+      });
+      console.log("Todo to be saved:", todo);
+  todo.save()
+  res.send("Hello World!")
+})
 
-app.get("/about", (req, res) => {
-  res.send("About Page" + req.priyu);
-});
 
-app.get("/contact", (req, res) => {
-  res.send("Contact Page");
-});
-app.listen(port, () => {
-  console.log(`Port is Running on ${port}`);
-});
+app.get('/a' , async (req, res)=>{
+    let todo = await Todo.findeOne({})
+    res.json({title: todo.title ,desc : todo.desc })
+})
+
+app.listen(Port, () => {
+  console.log(`Example app listening on port ${Port}`);
+
+  
+}) 
